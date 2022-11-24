3ad8ee86df68ca7d392eda66e54bb006548d1083
jinsa004 <jinsa004@gmail.com>
2022-11-24 14:35:17 +0900

    로그인세션값 테스트코드

--- src/main/java/shop/mtcoding/bank/web/UserApiController.java
+++ src/main/java/shop/mtcoding/bank/web/UserApiController.java
@@ -2,21 +2,31 @@ package shop.mtcoding.bank.web;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import shop.mtcoding.bank.config.auth.LoginUser;
 import shop.mtcoding.bank.dto.ResponseDto;
 import shop.mtcoding.bank.dto.UserReqDto.JoinReqDto;
 import shop.mtcoding.bank.dto.UserRespDto.JoinRespDto;
 import shop.mtcoding.bank.service.UserService;
 
 @RequiredArgsConstructor
+@RequestMapping("/api")
 @RestController
 public class UserApiController {
 
     private final UserService userService;
+
+    // @GetMapping("/user/session")
+    // public String userSession(@AuthenticationPrincipal LoginUser loginUser) {
+    // return "username : " + loginUser.getUsername();
+    // }
 
     @PostMapping("/join")
     public ResponseEntity<?> join(@RequestBody JoinReqDto joinReqDto) {
